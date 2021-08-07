4e06a51dbf090da4b85f8281b5d549d4440c3da2
borealis31 <jakeb.chouinard@gmail.com>
2021-08-06 22:53:46 -0400

    Creation of documentation for functions, including local functions including line-by-line narration. Optimization and velocity profiler edited to reduce repeated calculations and operations. Removal of commented/unused code from previous testing and debugging

--- discreteWaypointOptimizer.m
+++ discreteWaypointOptimizer.m
@@ -1,16 +1,51 @@
-function [waypointsOut, videoSetInter, videoSetPt, sFit] = discreteWaypointOptimizer(waypointsIn, widthNormalized, maxIterations, mindK, riseAllowance, bufferSize, title)
-%DISCRETEWAYPOINTOPTIMIZER Summary of this function goes here
-%   Detailed explanation goes here
+function [waypointsOut, videoSetInter, videoSetPt] = discreteWaypointOptimizer(waypointsIn, widthNormalized, maxIterations, mindK, riseAllowance, bufferSize, title)
+%DISCRETEWAYPOINTOPTIMIZER This script optimizes the input waypoints for curvature using an iterative patternsearch algorithm that focuses on localized curvature of a set of discrete points
+% -------------------------------------------------------------------------
+% 
+%   INPUTS: waypointsIn — Initial normalized set of waypoints (normalized 
+%                          center-points of track)
+%           widthNormalized — Width of track multiplied by normalizing 
+%                              factor from previous script
+%           maxIteration — Maximum number of iterations the optimizer will
+%                           run through before exiting
+%           mindK — Minimum reduction in K that we can experience across
+%                    iterations before exiting
+%           riseAllowance — Maximum amount the track's curvature can
+%                            increase across iterations without exiting
+%                            (precautionary)
+%           bufferSize — Percentage of track we reserve for a buffer region
+%                         to ensure that clipping does not occur between 
+%                         optimized track and original bounds
+%
+%   OUTPUTS: waypointsOut — Normalized, curvature optimized set of
+%                            waypoints
+%            videoSetInter — Cell array of normalized, curvature optimized
+%                             sets of waypoints in order of iteration, also
+%                             includes information on each set's iteration 
+%                             and curvature
+%            videoSetPt — Cell array of normalized, curvature optimized
+%                          sets of waypoints in order of POINTS for EACH 
+%                          iteration, also includes information on each 
+%                          set's iteration, curvature, and point position
+%                          after optimization
+%            sFit — The length of the new raceline following optimization
+% 
+% -------------------------------------------------------------------------
+%
+
+% Initialize variables for optimization script
 iteration = 1;
 pt = 1;
+dK = 1e2;
 kPrev = 1e3;
 minK = 1e4;
-dK = 1e2;
-
 rowCount = size(waypointsIn,1);
+halfTrackWidthAllowed = (widthNormalized/2)*(1-bufferSize);
 
+% Move waypointsIn value to waypointsOut to prevent overwrite
 waypointsOut = waypointsIn;
 
+% Initialize Video Set cell containers for future output efficiency
 videoSetInter = cell(maxIterations+1,4);
 videoSetInter{1,1} = waypointsIn;
 videoSetInter{1,2} = 0;
@@ -22,9 +57,17 @@ videoSetPt{1,2} = 0;
 videoSetPt{1,3} = 999;
 videoSetPt{1,4} = waypointsOut(1,:);
 
+% Begin optimization script and execute until a criteria fails
 while (iteration <= maxIterations) && abs(dK) > mindK && kPrev - minK < riseAllowance
+    % Repeat optimization for every point (excluding the LAST point, see
+    % below)
     for idxWP = 1:rowCount-1
+        % Select the current point as well as the 2 before (M1, M2) and 2
+        % after (P1, P2)
         switch idxWP
+            % SPECIAL CASES: The point being optimized is the 1st, 2nd,
+            % 3rd-to-last, or 2nd-to-last point. The LAST point is not
+            % used, as it is equal to the 1st point
             case 1
                 ptM2 = waypointsOut(end - 2, :);
                 ptM1 = waypointsOut(end - 1, :);
@@ -56,144 +99,140 @@ while (iteration <= maxIterations) && abs(dK) > mindK && kPrev - minK < riseAllo
                 ptP1 = waypointsOut(idxWP + 1, :);
                 ptP2 = waypointsOut(idxWP + 2, :);
         end
-        points = optim([ptP2; ptP1; ptOp; ptM1; ptM2], widthNormalized, waypointsIn, idxWP, bufferSize, iteration);
-%         points = optim2(waypointsOut, waypointsIn, widthNormalized, idxWP, bufferSize)
+        
+        % Call the optimizing script for the given set of points — see the
+        % local function declaration for more details
+        points = optim([ptP2; ptP1; ptOp; ptM1; ptM2], waypointsIn, idxWP, halfTrackWidthAllowed, iteration);
+        
+        % Update the point's values in the waypoint set for future
+        % iterations
         waypointsOut(idxWP, 1) = points(1);
         waypointsOut(idxWP, 2) = points(2);
         waypointsOut(end,:) = waypointsOut(1,:);
         
+        % Output Video Set point data 
         videoSetPt{pt+1,1} = waypointsOut;
         videoSetPt{pt+1,2} = iteration;
         videoSetPt{pt+1,3} = kPrev;
         videoSetPt{pt+1,4} = [waypointsOut(idxWP, 1), waypointsOut(idxWP, 2)];
         
+        % Progress point
         pt = pt + 1;
     end
 
-    arcLenAtPt = cumsum([0; sqrt(sum((waypointsOut(2:end,:)-waypointsOut(1:end-1,:)).^2,2))]);
-    X = spline(arcLenAtPt,waypointsOut(:,1));
-    Y = spline(arcLenAtPt,waypointsOut(:,2));
-    DX = fnder(X,1);
+    % Recalculate distance traveled at a point. Use these values as well as
+    % the curvature-optimized waypoints to generate splines so that we can 
+    % obtain DDX and DDY values at a point along the path using 
+    % fnder and ppval
+    sAtPt = cumsum([0; sqrt(sum((waypointsOut(2:end,:)-waypointsOut(1:end-1,:)).^2,2))]);
+    X = spline(sAtPt,waypointsOut(:,1));
+    Y = spline(sAtPt,waypointsOut(:,2));
     DDX = fnder(X,2);
-    DY = fnder(Y,1);
     DDY = fnder(Y,2);
 
-    curvature = @(s) (norm(cross([ppval(DX, s), ppval(DY, s), 0],[ppval(DDX, s), ppval(DDY, s), 0])))/(norm([ppval(DX, s), ppval(DY, s), 0]))^3;
-    kCurrent = integral(@(sIn) curvature(sIn), 0, arcLenAtPt(end),'ArrayValued',true);
+    % Define an equation for curvature in terms of distance traveled and
+    % integrate along the distance traveled to get the total curvature of
+    % the path
+    curvature = @(s) norm([ppval(DDX, s), ppval(DDY, s)]);
+    kCurrent = integral(@(sIn) curvature(sIn), 0, sAtPt(end),'ArrayValued',true);
+    
+    % Reset while loop comparison values for next iteration conditions
     dK = kPrev - kCurrent;
     kPrev = kCurrent;
+    
+    % Print out optimization progress based on iteration and curvature
     fprintf(['Iteration: ', num2str(iteration, '%.0f'), ' | k: ', num2str(kPrev, '%.8f'), newline]);
     
+    % Reset the vale for minK if the k of the new path is less than it
     if kPrev < minK
         minK = kPrev;
     end
     
-    sFit = arcLenAtPt(end);
     
+    % Output Video Set iteration data
     videoSetInter{iteration+1,1} = waypointsOut;
     videoSetInter{iteration+1,2} = iteration;
     videoSetInter{iteration+1,3} = kPrev;
     
+    % Progress iteration
     iteration = iteration + 1;
 end
+
+% Once iterations complete, remove empty cells from video sets and reshape
+% to original column size
 videoSetInter = reshape(videoSetInter(~cellfun('isempty',videoSetInter)),[],3);
 videoSetPt = reshape(videoSetPt(~cellfun('isempty',videoSetPt)),[],4);
+
 end
 
-function optimPtOut = optim(pointsIn, widthNormalized, waypointsIn, ptIdx, bufferSize, it)
+function optimPtOut = optim(pointsIn, waypointsIn, ptIdx, halfTrackWidthAllowed, iteration)
+%LOCALOPTIMPTOUT This local function uses discrete methods of calculating curvature to find new point positions via patternsearch optimization
+%
+% INPUTS: pointsIn — Set of local points in which the position of the
+%                     middle-most point is being optimized
+%         waypointsIn — Original set of waypoints used to determine the
+%                        chord along which a given point can be optimized
+%         ptIdx — The point number at which the script is currently
+%                  optimizing
+%         halfTrackWifthAllowed — Range that the point can shift along the
+%                                  chord based on normalized distance and
+%                                  allawble buffer from previous section
+%         iteration — The number of the current iteration of curvature
+%                      optimization
+%
+% OUTPUTS: optimPtOut — The new X,Y position of the optimized waypoint
+% -------------------------------------------------------------------------
+%
 
-    if ptIdx == size(waypointsIn,1)
-        ang = atan2(waypointsIn(1,2)-waypointsIn(ptIdx,2), waypointsIn(1,1)-waypointsIn(ptIdx,1));   
-    else
-        ang = atan2(waypointsIn(ptIdx+1,2)-waypointsIn(ptIdx,2), waypointsIn(ptIdx+1,1)-waypointsIn(ptIdx,1));   
-    end
-    
-    rotPointsIn = [(pointsIn(:,1) - waypointsIn(ptIdx,1))*cos(ang) + (pointsIn(:,2) - waypointsIn(ptIdx,2))*sin(ang), ...
-                   -(pointsIn(:,1) - waypointsIn(ptIdx,1))*sin(ang) + (pointsIn(:,2) - waypointsIn(ptIdx,2))*cos(ang)];
-
-    T2 = (rotPointsIn(1,:)-rotPointsIn(2,:))/sqrt(sum((rotPointsIn(1,:)-rotPointsIn(2,:)).^2,2));
-    T1 = @(y) (rotPointsIn(2,:)-[0, y])/sqrt(sum((rotPointsIn(2,:)-[0, y]).^2,2));
-    Tn1 = @(y) ([0, y]-rotPointsIn(4,:))/sqrt(sum(([0, y]-rotPointsIn(4,:)).^2,2));
-    Tn2 = (rotPointsIn(4,:)-rotPointsIn(5,:))/sqrt(sum((rotPointsIn(4,:)-rotPointsIn(5,:)).^2,2));
-    
-    N1 = @(y) T2-T1(y);
-    N0 = @(y) T1(y)-Tn1(y);
-    Nn1 = @(y) Tn1(y)-Tn2;
-    
-    K1 = @(y) norm(N1(y));
-    K0 = @(y) norm(N0(y));
-    Kn1 = @(y) norm(Nn1(y));
-    
-    fitnessFunction = @(y) (0.75*Kn1(y) + K0(y) + 0.75*K1(y));
-    
-    if it == 1
-        rotPointsIn(3,2) = rand(1)*(widthNormalized/2)*(1-bufferSize);
-    end
-    
-    [newYValue, ~, ~, ~] = patternsearch(@(y) fitnessFunction(y), rotPointsIn(3,2), [], [], [], [], (-widthNormalized/2)*(1-bufferSize), (widthNormalized/2)*(1-bufferSize), [], optimoptions(@patternsearch, 'MeshRotate', 'On', 'AccelerateMesh', true, 'Display', 'off'));
-    
-    optimPtOutRot = [0, newYValue];
-    
-    optimPtOut = optimPtOutRot*[cos(-ang) -sin(-ang); sin(-ang) cos(-ang)]+[waypointsIn(ptIdx,1), waypointsIn(ptIdx,2)];
-        
-end
-
-function optimPtOut = optim2(pointsIn, waypointsIn, widthNormalized, ptIdx, bufferSize)
-
+% Calculate the heading of the middle point so that the chord can be
+% created for optimization, accounting for the 1st and final point overlap
 if ptIdx == size(waypointsIn,1)
     ang = atan2(waypointsIn(1,2)-waypointsIn(ptIdx,2), waypointsIn(1,1)-waypointsIn(ptIdx,1));
 else
     ang = atan2(waypointsIn(ptIdx+1,2)-waypointsIn(ptIdx,2), waypointsIn(ptIdx+1,1)-waypointsIn(ptIdx,1));
 end
 
+% Rotate and center all five points so that the chord for optimization is
+% centered at the y-axis
 rotPointsIn = [(pointsIn(:,1) - waypointsIn(ptIdx,1))*cos(ang) + (pointsIn(:,2) - waypointsIn(ptIdx,2))*sin(ang), ...
     -(pointsIn(:,1) - waypointsIn(ptIdx,1))*sin(ang) + (pointsIn(:,2) - waypointsIn(ptIdx,2))*cos(ang)];
 
-ptCount = length(pointsIn);
+% Create expressions for tangent vectors (1st derivatives) in terms of y
+% (dR/ds == T)
+T2 = (rotPointsIn(1,:)-rotPointsIn(2,:))/sqrt(sum((rotPointsIn(1,:)-rotPointsIn(2,:)).^2,2));
+T1 = @(y) (rotPointsIn(2,:)-[0, y])/sqrt(sum((rotPointsIn(2,:)-[0, y]).^2,2));
+Tn1 = @(y) ([0, y]-rotPointsIn(4,:))/sqrt(sum(([0, y]-rotPointsIn(4,:)).^2,2));
+Tn2 = (rotPointsIn(4,:)-rotPointsIn(5,:))/sqrt(sum((rotPointsIn(4,:)-rotPointsIn(5,:)).^2,2));
 
-if ptIdx == 1
-    rotPointsInFn = @(y) [0,y; rotPointsIn(2:end,:)];
-elseif ptIdx == ptCount
-    rotPointsInFn = @(y) [rotPointsIn(1:end-1,:); 0,y];
-else
-    rotPointsInFn = @(y) [rotPointsIn(1:ptIdx-1,:); 0,y; rotPointsIn(ptIdx+1:end,:)];
+% Use tangent vectors to create expressions for normal vectors (2nd
+% derivatives) in terms of y (dT/ds == N)
+N1 = @(y) T2-T1(y);
+N0 = @(y) T1(y)-Tn1(y);
+Nn1 = @(y) Tn1(y)-Tn2;
+
+% Use dT/ds curvature equation to create expressions for curvature in terms
+% of y (K == magnitude(dT/ds))
+K1 = @(y) norm(N1(y));
+K0 = @(y) norm(N0(y));
+Kn1 = @(y) norm(Nn1(y));
+
+% Use the three curvature expressions to create a fitness function for the
+% patternsearch algorithm, weighting the curren point's curvature slightly
+% more than the immediate previous and future curvatures
+fitnessFunction = @(y) (0.75*Kn1(y) + K0(y) + 0.75*K1(y));
+
+% If this is the first iteration, make the initial y value for the
+% patternseach a random number within track constraints
+if iteration == 1
+    rotPointsIn(3,2) = rand(1)*halfTrackWidthAllowed;
 end
 
-arcLenDiff = @(y) [0; sqrt(sum((subsref(rotPointsInFn(y), struct('type', '()', 'subs', {{2:ptCount,':'}})) - subsref(rotPointsInFn(y), struct('type', '()', 'subs', {{1:ptCount-1,':'}}))).^2,2))];
-arcLenAtPtFn = @(y) cumsum(arcLenDiff(y));
+% Implement patternsearch algorithm to return a y value that optimizes the
+% localized curvature and suppresses its own output
+[newYValue, ~, ~, ~] = patternsearch(@(y) fitnessFunction(y), rotPointsIn(3,2), [], [], [], [], -halfTrackWidthAllowed, halfTrackWidthAllowed, [], optimoptions(@patternsearch, 'MeshRotate', 'On', 'AccelerateMesh', true, 'Display', 'off'));
 
-%     arcLenAtPt = @(y) cumsum([0; sqrt(sum((waypointsOut(2:end,:)-waypointsOut(1:end-1,:)).^2,2))]);
-X = @(y) spline(arcLenAtPtFn(y),subsref(rotPointsInFn(y), struct('type', '()', 'subs', {{':',1}})));
-Y = @(y) spline(arcLenAtPtFn(y),subsref(rotPointsInFn(y), struct('type', '()', 'subs', {{':',2}})));
-DX = @(y) fnder(X(y),1);
-DDX = @(y) fnder(X(y),2);
-DY = @(y) fnder(Y(y),1);
-DDY = @(y) fnder(Y(y),2);
-
-curvature = @(y, sIn) norm([ppval(DDX(y), sIn), ppval(DDY(y), sIn)])/norm([ppval(DX(y), sIn), ppval(DDY(y), sIn)]);
-fitnessFunction = @(y) integral(@(sIn) 0.001*(curvature(y, sIn))^2, 0, subsref(arcLenAtPtFn(y), struct('type', '()', 'subs', {{ptCount}})), 'WayPoints', arcLenAtPtFn(y), 'ArrayValued',true,'AbsTol',1e-4,'RelTol',1e-2);
-
-%[newYValue, ~, ~, out] = ga(@(y) fitnessFunction(y), 1, [], [], [], [], (-widthNormalized/2)*(1-bufferSize), (widthNormalized/2)*(1-bufferSize), [], optimoptions(@ga, 'MaxTime',20));
-[newYValue, ~, ~, out] = patternsearch(@(y) fitnessFunction(y), rotPointsIn(ptIdx,2), [], [], [], [], (-widthNormalized/2)*(1-bufferSize), (widthNormalized/2)*(1-bufferSize), [], optimoptions(@patternsearch,'Cache','off'));
-
-optimPtOutRot = [0, newYValue];
-
-optimPtOut = optimPtOutRot*[cos(-ang) -sin(-ang); sin(-ang) cos(-ang)]+[waypointsIn(ptIdx,1), waypointsIn(ptIdx,2)];
+% Unrotate and retranslate new point to be stored in place of previous 
+% point being optimized 
+optimPtOut = [0, newYValue]*[cos(-ang) -sin(-ang); sin(-ang) cos(-ang)]+[waypointsIn(ptIdx,1), waypointsIn(ptIdx,2)];
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- velocityProfilerV2.m
+++ velocityProfilerV2.m
@@ -1,13 +1,37 @@
-function [vProfile, vProfByS, vCrit ,minTLap] = velocityProfilerV2(timeStep, waypointsIn, lapsQty, ftMax, fnMax, mass)
-%UNTITLED Summary of this function goes here
-%   Based on:
+function [vProfile, bestLapSet] = velocityProfilerV2(timeStep, waypointsIn, lapsQty, ftMax, fnMax, mass)
+%VELOCITYPROFILERV2 Velocity Profiler V2 uses corrected point-mass kinematics from the below thesis to generate a time-velocity profile that is optimized for performance
+% -------------------------------------------------------------------------
+% Based on:
 % Friedl, Tyler, "Optimized Trajectory Generation for Car-Like Robots on a Closed Loop Track" (2017).
 % Electronic Theses and Dissertations. 1370.
 % https://digitalcommons.du.edu/etd/1370
+%
+% -------------------------------------------------------------------------
+% 
+% INPUTS: timeStep — Resolution of time by which to determine velocity
+%                     profile
+%         waypointsIn — Optimized curvature path
+%         lapsQty — Number of laps to calculate velocty profile for
+%         ftMax — Maximum tangential force constraint
+%         fnMax — Maximum normal (centripetal) force constraint
+%         mass — Mass of the point mass
+%
+% OUTPUT: vProfile — 8-Column matrix detailing the velocity profile of the
+%                     point-mass [velocity, time, cumulative distance, X,
+%                     Y, tangential force, normal force, lap number]
+%         bestLapSet — 4-Column matric detailing the velocity profile of
+%                       the point-mass during the middle-most lap
+%                       [velocity, time, X, Y]
+% 
+% -------------------------------------------------------------------------
+%
 
+% Initialize constants used in kinematic equations to reduce calculations
 ftFactor = ftMax^2;
 ftfnFactor = (ftMax/fnMax)^2;
 
+% If we want multiple laps, concatenate the lap points excluding
+% overlapping points
 for idxLap = 1:lapsQty
     if idxLap == 1
         waypointsTracked = waypointsIn;
@@ -16,13 +40,17 @@ for idxLap = 1:lapsQty
     end
 end
 
+% Initialize number of points and number of profiles
 numPoints = length(waypointsTracked);
 numProfiles = 1;
 
+% Calculate distance traveled between each point as well as distance
+% traveled cumulatively at each point
 sDiff = ([0; sqrt(sum((waypointsTracked(2:end,:)-waypointsTracked(1:end-1,:)).^2,2))]);
 sAtPt = cumsum(sDiff);
 
-
+% Normalize waypoints and distances to ensure that points along path are
+% evenly spaced
 normFactor = floor(sAtPt(end))/sAtPt(end);
 sNormalized = sAtPt.*normFactor;
 waypointsTrackedNormalized = waypointsTracked.*normFactor;
@@ -31,31 +59,41 @@ waypointsTracked = [spline(sNormalized, waypointsTrackedNormalized(:,1),0:0.01:s
 sDiff = ([0; sqrt(sum((waypointsTracked(2:end,:)-waypointsTracked(1:end-1,:)).^2,2))]);
 sAtPt = cumsum(sDiff);
 
-
-
-
-
+% Calculate a ds value for use to calculate finer X, Y, DDX, and DDY
+% values across the path later on
 ds = sAtPt(end)/(10*numPoints);
 
 X = spline(sAtPt,waypointsTracked(:,1));
 Y = spline(sAtPt,waypointsTracked(:,2));
 
-DX = fnder(X,1);
 DDX = fnder(X,2);
-DY = fnder(Y,1);
 DDY = fnder(Y,2);
 
+% Create a high-definition (HD) set of cumulative distance-travelled values
 sHD = (0:ds:sAtPt(end))';
 
-rTrack = 1./kAtPoint(sHD, DX, DDX, DY, DDY);
+% Pass cumulative distances as well as DDX and DDY splines to the local
+% curvature function to calculate radii of curvature across the track. Use
+% these values to create a spline for radius of curvature values. See the
+% local function for more information
+rTrack = 1./kAtPoint(sHD, DDX, DDY);
 rInterp = spline(0:ds:sAtPt(end), rTrack);
 
+% Calculate maximum velocities along the path by radius of curvature and
+% force constraints
 vCrit = sqrt(ppval(rInterp,sHD).*(fnMax/mass));
 
+% Find local minima on the vCrit array and return a 2-column matrix
+% [cumulative distance traveled, critical velocity at cumulative distance
+% traveled]
 vCritMins = [sHD(islocalmin(vCrit,'FlatSelection','center','MaxNumExtrema',floor(sAtPt(end)/ds))), vCrit(islocalmin(vCrit,'FlatSelection','center','MaxNumExtrema',floor(sAtPt(end)/ds)))];
 
-profiles = cell(length(vCritMins)+1, 1);
+% Initialize number of profiles needed to create cumulative profile
+profiles = cell(length(vCritMins)+2, 1);
 
+% For each minima, generate a velocity profile based on kinematics
+% equations. Additionally, generate a velocity profile for initial and final
+% constraints (vo = 0, vf = 0). See local function for more information
 for idxMin = 1:length(vCritMins)+2
     if idxMin == length(vCritMins)+1
         profiles{idxMin} = generateVByS(ds, [0,0], numProfiles, sAtPt, rInterp, ftFactor, ftfnFactor, mass);
@@ -67,28 +105,38 @@ for idxMin = 1:length(vCritMins)+2
     numProfiles = numProfiles + 1;
 end
 
+% Initialize composite array of velcoity profiles and select the minimum
+% velocity values for each distance traveled to create optimized velocity
+% profile
 vProfComposite = [];
 for idxProf = 1:numel(profiles)
     vProfComposite = [vProfComposite, profiles{idxProf}];
 end
-
 vProfByS = min(vProfComposite,[],2);
 
-v = [0];
+% Initalize time-velocity profile values. NOTE: the initial starting
+% position is ds rather than 0, as starting at 0 results in a long pause at
+% the beginning of the time-velocity profile
+v = 0;
 vIdx = 1;
-t = [0];
-sTraveled = [ds];
+t = 0;
+sTraveled = ds;
 lap = 1;
 ft = 0;
 fn = 0;
 
+% Create a velocity spline in terms of distance traveled
 vInterp = spline(0:ds:sAtPt(end), vProfByS);
 
+% Start timer for time-velocity calculations
 vTime = tic;
 fprintf(['Beginning Time-Velocty Calculations...' newline])
+
+% While the distance traveled is less than the total distance, run state
+% updaters and velocity calculations. Store data at each time point in a
+% variety of arrays
 while (sAtPt(end) - sTraveled(end)) > 0.001
-    sTraveled(end+1,1) = stateRefresher(sTraveled(vIdx,1), v(vIdx,1), timeStep);
-    % fprintf(['Distance Traveled Along Track: %f m' newline],sTraveled(end));
+    sTraveled(vIdx+1,1) = sTraveled(vIdx,1) + v(vIdx)*timeStep;
     vIdx = vIdx + 1;
     v(vIdx,1) = ppval(vInterp, sTraveled(end));
     t(vIdx,1) = t(vIdx-1,1) + timeStep;
@@ -97,26 +145,45 @@ while (sAtPt(end) - sTraveled(end)) > 0.001
     lap(vIdx,1) = ceil((sTraveled(end)/sAtPt(end))*lapsQty);
 end
 fprintf(['Time-Velocity Calculations Complete: %f sec' newline], toc(vTime))
-vVsT = spline(t, v);
 
-
-% ft = abs(mass*(ppval(fnder(vVsT,1),t)));
-% fn = mass*((ppval(vVsT,t)).^2)./(ppval(rInterp,sTraveled));
-
-vCrit = [(0:ds:sAtPt(end))', vCrit];
+% Compile arrays into velocity profile output
 vProfile = [v, t, sTraveled, ppval(X, sTraveled), ppval(Y, sTraveled), ft, fn, lap];
-tBestLapSet = t(lap == ceil(lapsQty/2));
-minTLap = tBestLapSet(end) - tBestLapSet(1);
+
+% Find the logic map of all values that exist during the middle-most lap
+% and record them in the bestLapSet output variable
+bestLapMap = lap == ceil(lapsQty/2);
+bestLapSet = [vProfile(bestLapMap,1), vProfile(bestLapMap,2), vProfile(bestLapMap,4), vProfile(bestLapMap,5)];
 
 end
 
 function prof = generateVByS(ds, critMins, numProfiles, sIn, rInterp, ftFactor, ftfnFactor, mass)
+%LOCALGENERATEVBYS This local function generates a profile for velocity in terms of distance traveled
+% -------------------------------------------------------------------------
+%
+% INPUTS: ds — Distance steps by which to calculate the profile
+%         critMins — The point of the critical minimum [S, vCrit]
+%         numProfiles — Current profile number
+%         sIn — Distance traveled array
+%         rInterp — Radius of curvature spline in terms of distance
+%                   traveled
+%         ftFactor — Constant for calculations based on known
+%                     characteristics
+%         ftfnFactor — Constant for calculations based on known
+%                       characteristics
+%         mass — Mass of the point-mass
+%
+% OUTPUTS: prof — Velocity profile in terms of distance traveled [S, V]
+%
+% -------------------------------------------------------------------------
 
-vDecel = [critMins(2)];
-vAccel = [critMins(2)];
+% Initialize arrays for acceleration and deceleration profiles
+vDecel = critMins(2);
+vAccel = critMins(2);
 decelS = (critMins(1):-ds:0)';
 accelS = (critMins(1):ds:sIn(end))';
 
+% Eliminate accelaration and deceleration S value arrays if any are above
+% maximum or below minimum
 if any(accelS > sIn(end))
     accelS = [];
 end
@@ -125,45 +192,62 @@ if any(decelS < 0)
     decelS = [];
 end
 
+% Set previous velocity value
 vPrev = critMins(2);
 
+% For each ds point between the critical point's S and 0, calculate the
+% velocity value subject to kinematic constraints
 for idxDecel = 2:length(decelS)
-%     vNew = real(sqrt(((-2*(decelS(idxDecel)-decelS(idxDecel-1))*sqrt(ftFactor-(mass/(ppval(rInterp,decelS(idxDecel-1))))*ftfnFactor*(vPrev^2)))/(mass))+vPrev^2));
     vNew = real(sqrt((vPrev^2)+((2*abs(decelS(idxDecel)-decelS(idxDecel-1))/mass)*sqrt(ftFactor - ftfnFactor*((mass*vPrev^2)/ppval(rInterp,decelS(idxDecel-1)))^2))));
     vPrev = vNew;
     vDecel(end+1,1) = vPrev;
 end
 
+% Reset previous velocty value
 vPrev = critMins(2);
 
+% For each ds point between the critical point's S and max S, calculate the
+% velocity value dubject to kinematic constraints
 for idxAccel = 2:length(accelS)
-%     vNew = real(sqrt(((2*(accelS(idxAccel)-accelS(idxAccel-1))*sqrt(ftFactor-(mass/(ppval(rInterp,accelS(idxAccel))))*ftfnFactor*(vPrev^2)))/(mass))+vPrev^2));
     vNew = real(sqrt((vPrev^2)+((2*abs(accelS(idxAccel)-accelS(idxAccel-1))/mass)*sqrt(ftFactor - ftfnFactor*((mass*vPrev^2)/ppval(rInterp,accelS(idxAccel)))^2))));
     vPrev = vNew;
     vAccel(end+1,1) = vPrev;
 end
 
 
-
-profProc = [flip(vDecel,1); vAccel(2:end)];
-% profProcSplineNormalizer = spline([flip(decelS,1); accelS(2:end,1)], real(profProc));
-% prof = [ppval(profProcSplineNormalizer, (0:0.01:sIn(end))'), (0:0.01:sIn(end))'];
-prof = [profProc, [flip(decelS,1); accelS(2:end,1)]];
+% Compile results into a new matrix for outputting velocity profile in
+% terms of S
+prof = [[flip(vDecel,1); vAccel(2:end)], [flip(decelS,1); accelS(2:end,1)]];
 fprintf(['Profile ', num2str(numProfiles, '%.0f'), ' Generated...' newline]);
-if numel(accelS) == 0 || numel(decelS) == 0
-    prof = prof(1:end-1,:);
-end
+
+% If either of these are empty, remove the ending entry (COMMENTED OUT
+% BECAUSE IT IS UNNECESSARY)
+% if numel(accelS) == 0 || numel(decelS) == 0
+%     prof = prof(1:end-1,:);
+% end
+
 
 end
 
-function sOut = stateRefresher(sIn, vIn, tStep)
-sOut = sIn + vIn*tStep;
-end
+function kSection = kAtPoint(sIn, DDX, DDY)
+%LOCALKATPOINT This local function finds and returns the curvature at a given point in terms of total distance traveled
+% -------------------------------------------------------------------------
+%
+% INPUT: sIn — Set of cumulative distances traveled to determine 
+%               curvatures for
+%        DDX — DDX spline in terms of cumulative distance traveled
+%        DDY — DDY spline in terms of cumulative distance traveled
+%
+% OUTPUT: kSetion — Set of curvatures returned for input distances
+%
+% -------------------------------------------------------------------------
 
-function kSection = kAtPoint(sIn, DX, DDX, DY, DDY)
+% Initialize an array of curvature values
 kSection = zeros(length(sIn), 1);
+
+% For each distance traveled value, calculate and store curvature in teh
+% curvature array
 for idxS = 1:length(sIn)
-    %     kSection(idxS) = norm(cross([ppval(DX, sIn(idxS)), ppval(DY, sIn(idxS)), 0],[ppval(DDX, sIn(idxS)), ppval(DDY, sIn(idxS)), 0]))/norm([ppval(DX, sIn(idxS)), ppval(DY, sIn(idxS))]);
     kSection(idxS) = norm([ppval(DDX, sIn(idxS)), ppval(DDY, sIn(idxS)), 0]);
 end
 end
